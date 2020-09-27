f6bd4be445962af842e940971cb1c2d9272a3d65
Pranav-Official <50313731+Pranav-Official@users.noreply.github.com>
2020-09-27 11:57:21 +0530

    Update ANN scraper.py

--- ANN scraper.py
+++ ANN scraper.py
@@ -1,45 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
 
 
 from bs4 import BeautifulSoup as bs4
-
-
-# In[10]:
-
-
 import requests
 
-
-# In[11]:
-
-
 url = "https://www.animenewsnetwork.com/"
-
-
-# In[12]:
-
-
 html = requests.get(url).text
-
-
-# In[13]:
-
-
 main_soup = bs4(html,"html.parser")
-
-
-# In[14]:
-
-
 news_box = main_soup.find_all('div',{"class":"herald box news"})
-
-
-# In[15]:
-
-
 count = 0
 for box in news_box:
     count+=1
@@ -51,11 +18,4 @@ for box in news_box:
     page_url = box.find('div',{"class":"wrap"}).div.h3.a['href']
     print(str(count) + "\n\n" , title + "\n\n" , time + "\n\n", descript + "\n\n", url + page_url[1:]+"\n\n\n")
     
-    
-
-
-# In[ ]:
-
-
-
 
